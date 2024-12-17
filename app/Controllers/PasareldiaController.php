cd50077b42f348188aa48d0a029db415eb5ae0cf
Guillemv92 <117411545+Guillemv92@users.noreply.github.com>
2024-12-17 15:42:25 -0300

    Update PasareldiaController.php

--- app/Controllers/PasareldiaController.php
+++ app/Controllers/PasareldiaController.php
@@ -6,6 +6,9 @@ use App\Models\Servicio;
 
 class PasareldiaController {
     public function mostrarFormulario() {
+          if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
         if (!isset($_SESSION['user'])) {
             header("Location: /login");
             exit();
